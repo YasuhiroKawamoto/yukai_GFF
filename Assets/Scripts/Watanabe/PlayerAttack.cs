12c609ae74667515fa4ee8347c80c2fb4ddfada5
hetima333 <hetima333@gmail.com>
2016-12-01 23:06:09 +0900

    攻撃ボタンを押すととりあえずhitとログがでるように

--- Assets/Scripts/Watanabe/PlayerAttack.cs
+++ Assets/Scripts/Watanabe/PlayerAttack.cs
@@ -1,45 +1,41 @@
 ﻿// ---------------------------------------
 // File: PlayerAttack.cs
 // 
-// Date: 
+// Date: 2016/12/01
 // 
 // Author: Y.Watanabe
 // ---------------------------------------
 
 using UnityEngine;
 using System.Collections;
+using UniRx.Triggers;
+using UniRx;
 
 public class PlayerAttack : MonoBehaviour
 {
 	#region variable
-	
+
+	//IntReactiveProperty deadStream = new IntReactiveProperty(10);
+
+	//public IObservable<int> DeadStream
+	//{
+	//	get { return deadStream.AsObservable().Where(hp => hp <= 0); }
+	//}
+
 	#endregion
-	
+
 	#region method
-	/// <summary> 
-	/// 初期化処理
-	/// </summary>
-	void Awake ()
-	{
-	
-	}
-	
+
 	/// <summary> 
 	/// 更新前処理
 	/// </summary>
 	void Start ()
 	{
-	
+		this.UpdateAsObservable()
+			.Where(_ => Input.GetButtonDown("Fire1"))
+			.Subscribe(_ => Debug.Log("fire"));
+
 	}
 
-	
-	/// <summary> 
-	/// 更新処理
-	/// </summary>
-	void Update ()
-	{
-	
-	}
-	
 	#endregion
 }
