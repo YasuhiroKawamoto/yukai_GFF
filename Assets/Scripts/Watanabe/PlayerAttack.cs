4759d77b87d0fec184bc8e510eeb4fbc3645abd1
hetima333 <hetima333@gmail.com>
2016-12-01 19:22:47 +0900

    Scripts/WatanabeにPlayerAttackを追加

---
+++ Assets/Scripts/Watanabe/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------
+// File: PlayerAttack.cs
+// 
+// Date: 
+// 
+// Author: Y.Watanabe
+// ---------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour
+{
+	#region variable
+	
+	#endregion
+	
+	#region method
+	/// <summary> 
+	/// 初期化処理
+	/// </summary>
+	void Awake ()
+	{
+	
+	}
+	
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start ()
+	{
+	
+	}
+
+	
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void Update ()
+	{
+	
+	}
+	
+	#endregion
+}
