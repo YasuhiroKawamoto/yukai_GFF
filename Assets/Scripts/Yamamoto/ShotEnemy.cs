ded3358ccd51f1738e129891ff0f872b3667bae2
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2016-12-01 20:36:54 +0900

    敵４の作成

--- Assets/Scripts/Yamamoto/ShotEnemy.cs
+++ Assets/Scripts/Yamamoto/ShotEnemy.cs
@@ -1,17 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShotEnemy : MonoBehaviour {
+public class ShotEnemy : MonoBehaviour
+{
+    [SerializeField ,Range(0,10)]
+    private int hp_;          // 体力
+    [SerializeField]
+    private int atk_;         // 攻撃力(接触ダメージ)
+    [SerializeField]
+    private GameObject bullet_;  // 弾
+    [SerializeField,Range(0.1f,5.0f)]
+    private float bulletDelayTime__ = 3.0f;  // 弾待機時間
 
-	// Use this for initialization
-	void Start ()
+    // Startをコルーチンで呼ぶ
+    IEnumerator Start()
     {
-	
-	}
-	
-	// Update is called once per frame
+        bool init = true;
+
+        while (true)
+        {
+            if (init)
+            {
+                init = false;
+            }
+            else
+            {
+                // 弾を作成
+                Instantiate(bullet_, transform.position, transform.rotation);
+            }
+
+            // 3.0秒待つ
+            yield return new WaitForSeconds(bulletDelayTime__);
+        }
+    }
+
+
+    // 更新処理
 	void Update ()
     {
-	
-	}
+        // 消滅判定
+        if (hp_ <= 0)
+        {
+            // 消滅処理
+            Destroy(gameObject);
+        }
+    }
+
+    // 向きを取得
 }
