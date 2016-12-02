9cccc2139e232eb24ffbdd8e42d22f2f55918de2
konji <konjikk15@gmail.com>
2016-12-02 22:15:09 +0900

    プレイヤーのジャンプ

--- Assets/Scripts/Koshida/PlayerMove.cs
+++ Assets/Scripts/Koshida/PlayerMove.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class PlayerMove : MonoBehaviour {
 
     [SerializeField]
-    private float speed = 10.0f;
+    private float speed = 10.0f;        //移動スピード
 
     [SerializeField]
-    private float jump = 100.0f;
+    private float jump_height = 11.0f;  //ジャンプの高さ
 
     private Rigidbody2D rb2D;
+
+    private bool move;                  //移動
+
+    private int jump_count = 0;         //ジャンプ回数
+
+    public bool is_jump = false;        //ジャンプ
+
+    float x;
 
     void Start()
     {
@@ -18,10 +26,7 @@ public class PlayerMove : MonoBehaviour {
 
     void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-
-        //左か右を入力したら
-        if (x != 0)
+        if(move)
         {
             //入力方向へ移動
             rb2D.velocity = new Vector2(x * speed, rb2D.velocity.y);
@@ -30,11 +35,42 @@ public class PlayerMove : MonoBehaviour {
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
         }
+    }
 
-        //Zキーを入力したら
-        if (Input.GetKeyDown(KeyCode.Z))
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+
+        //左か右を入力したら
+        if (x != 0)
         {
-            //rb2D.velocity = new Vector2(0,)
+            move = true;
         }
+        else
+        {
+            move = false;
+        }
+
+        //ジャンプ回数が2回未満で、ジャンプ入力したら
+        if (jump_count < 2 && Input.GetButtonDown("Jump"))
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jump_height);
+            is_jump = true;
+            jump_count++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        // レイヤー名を取得
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        //地面に着地したら
+        if (layerName == "Ground")
+        {
+            is_jump = false;
+            jump_count = 0;
+        }
+
     }
 }
